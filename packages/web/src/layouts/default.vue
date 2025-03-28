39ce28e9f9967bbb802a0c0114a12356f66b93f2
Artur Lissin <artur.lissin@dsmz.de>
2025-03-28 10:18:45 +0000

    feat: add NoScript

--- packages/web/src/layouts/default.vue
+++ packages/web/src/layouts/default.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { AppFooter, AppHeader } from '#components';
+import { AppFooter, AppHeader, NoScript } from '#components';
 </script>
 
 <template>
     <div>
+        <NoScript>Enable JavaScript</NoScript>
         <AppHeader />
         <slot />
         <AppFooter />
