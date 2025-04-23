50c89693edd7aad3670c90de94d7201213803099
artdotlis <artdotlis@pm.me>
2025-04-23 16:51:34 +0000

    fix: define layout in error differently

--- packages/web/src/error.vue
+++ packages/web/src/error.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app';
 import { clearError } from '#app';
-import { definePageMeta } from '#imports';
 import UiRoutes from '#shared/constants/route/ui';
 
 defineProps({
@@ -12,16 +11,16 @@ defineProps({
 });
 const to = () => clearError({ redirect: UiRoutes.root });
 
-definePageMeta({
-    layout: 'error',
-});
+const layout = 'error';
 </script>
 
 <template>
-    <div>
-        <h1>{{ error?.statusCode ?? 500 }}</h1>
-        <button @click="to">
-            Go back home
-        </button>
-    </div>
+    <NuxtLayout :name="layout">
+        <div>
+            <h1>{{ error?.statusCode ?? 500 }}</h1>
+            <button @click="to">
+                Go back home
+            </button>
+        </div>
+    </NuxtLayout>
 </template>
